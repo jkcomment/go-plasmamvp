9a6bb06026b69cf85c1f13c1844a14389514f8ba
yuzushioh <yuzushioh@gmail.com>
2018-05-04 22:23:21 +0900

    delete plasma.Plasma

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/yuzushioh/go-plasmamvp/plasma"
 )
 
 func main() {
 	fmt.Println("Hello, Plasma")
-
-	plasma := plasma.Plasma{}
-	fmt.Print(plasma)
 }
