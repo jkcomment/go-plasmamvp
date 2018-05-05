759e00bdcacdd0486498664df3d106c45dd46067
yuzushioh <yuzushioh@gmail.com>
2018-05-05 20:08:35 +0900

    add more error handilng for command line args

--- root_chain/deploy.go
+++ root_chain/deploy.go
@@ -12,14 +12,21 @@ func main() {
 
 	if err := deployer.Dial("http://localhost:8545"); err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
+		return
 	}
 
 	prvkey := os.Args[1]
+	if len(prvkey) > 1 {
+		fmt.Fprintf(os.Stdout, "private key is necessary for the deploy")
+		return
+	}
+
 	deployer.SetPrvKey(prvkey)
 
 	addr, err := deployer.Deploy(contract.RootChainABI, contract.RootChainBin)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
+		return
 	}
 
 	fmt.Println(addr.String())
