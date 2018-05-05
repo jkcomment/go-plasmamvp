e58b09b94e9df67816870343433c6fcc7ac4cefe
Shota Yamamota <1230124fw@gmail.com>
2018-05-06 01:52:22 +0900

    add function of SprintTransaction for debugging

--- lib/lib.go
+++ lib/lib.go
@@ -1,8 +1,12 @@
 package lib
 
 import (
+	"bytes"
+	"encoding/json"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
@@ -11,4 +15,17 @@ func SetPrvKey(prvkeyHex string) *bind.TransactOpts {
 	keyBytes := common.FromHex(prvkeyHex)
 	key := crypto.ToECDSAUnsafe(keyBytes)
 	return bind.NewKeyedTransactor(key)
+}
+
+func SprintTransaction(tx *types.Transaction) (string, error) {
+	txbody, err := tx.MarshalJSON()
+	if err != nil {
+		return "", err
+	}
+	var txjson bytes.Buffer
+	if err := json.Indent(&txjson, txbody, "", "\t"); err != nil {
+		return "", err
+	}
+
+	return string(txjson.Bytes()), nil
 }
